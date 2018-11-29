a9650f6e76a077311d4086641d166a4438690821
SKITTLE WAN (INFRA-ISD-OOCLL/HKG) <WANSK@OOCL.COM>
2018-11-29 17:15:11 +0800

    finish

---
+++ src/main/java/com/tw/apistackbase/core/Employee.java
@@ -0,0 +1,51 @@
+package com.tw.apistackbase.core;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private int age;
+
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    public Employee() {}
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
